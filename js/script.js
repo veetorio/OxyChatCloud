48ae3cb835605cc680b2f1be504798ba084e3f2a
Veetorio <ettorevitorio98@gmail.com>
2024-02-03 16:44:41 -0300

    commit : deploy na vercel e js implementado

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const links = Object.values(document.getElementsByClassName('links'))
+
+
+links.forEach(link => {
+    link.addEventListener('click', event => {
+        const id = event.target.id
+        switch(id){
+            case 'link1' :
+                open('https://github.com/veetorio')
+                break
+            case 'link2' :
+                open('https://www.linkedin.com/in/ettore-vitorio-b38135280/')
+                break
+            case 'link3' :
+                open('https://www.instagram.com/ettorevit_/') 
+                break
+        }
+    })
+})
+
+
+
